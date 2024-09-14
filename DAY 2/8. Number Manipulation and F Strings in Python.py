1ba835c0e48b224b5d9a4628d4f98ac3ee4eaaf5
saifurrhman <132482254+saifurrhman@users.noreply.github.com>
2024-09-14 17:39:35 +0500

    days-2

---
+++ DAY 2/8. Number Manipulation and F Strings in Python.py
@@ -0,0 +1,5 @@
+print(round(2.7893456789,1))
+score=0
+score +=1
+iswinning=True
+print("your score is ")
---
+++ DAY 2/9. [Interactive Coding Exercise] Life in Weeks.py
@@ -0,0 +1,8 @@
+age=input("What is curent age ")
+age=int(age)
+years=90-age
+month=years*12
+days=years*365
+week=years*52
+answer=f"you have a {years} years ,{month} months ,{week} weeks,  and {days} days left"
+print(answer)
