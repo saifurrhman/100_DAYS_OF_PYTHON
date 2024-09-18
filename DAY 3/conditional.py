edc7800ad9dcd1095c653336b7613e4d72a543da
saifurrhman <132482254+saifurrhman@users.noreply.github.com>
2024-09-18 18:03:12 +0500

    DAY_3

---
+++ DAY 3/conditional.py
@@ -0,0 +1,6 @@
+print("SAIF UR REHMAN")
+heigth=int(input( "Your cgpa "  ))
+if heigth>3.99:
+    print("you can easly apply your us")
+else:
+    print("you cannot easly apply your us")
---
+++ DAY 3/evenand odd .py
@@ -0,0 +1,6 @@
+number=int(input("YOU ENTER THE NUMBER:"))
+
+if number%2==0:
+    print("THIS IS EVEN NUMBER")
+else: 
+    print("THIS IS ODD NUMBER") 
