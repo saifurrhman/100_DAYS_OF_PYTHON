0667bd9b41e4e20ba5b05bc2b07d5fcf3e819970
saifurrhman <132482254+saifurrhman@users.noreply.github.com>
2024-09-24 18:25:30 +0500

    day-4

---
+++ DAY 4/4. Understanding the Offset and Appending Items to Lists.py
@@ -0,0 +1,2 @@
+state_of_pakistan=["PUNJAB","REHMAN","FAWAD","SANA","PUNJAB","ALI","KPK"]
+print(state_of_pakistan[2])
---
+++ DAY 4/5. [Interactive Coding Exercise] Banker Roulette - Who will pay the bill.py
@@ -0,0 +1,10 @@
+import random
+test_seed=int(input("CRETE A SEED NUMBER:"))
+random.seed(test_seed)
+nameasCSV=input("GIVE ME EVERYBOBY NAMES,SEPARETE BY A COMMMA  ")
+NAME=nameasCSV.split(" , ")
+# random.randint(0,9)
+print(len(NAME))
+num_items=len(nameasCSV)
+random_choice=random.randint(0,num_items-1)
+print(random_choice)
